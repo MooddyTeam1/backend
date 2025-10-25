4db6e7eaefdfdf77643692ddccae031a2380f164
sdw98 <sdw9851@gmail.com>
2025-10-26 00:30:09 +0900

    feat: 유저 온보딩 선호 장르/아티스트 구현 및 시드데이터 추가

--- src/main/java/com/mooddy/backend/feature/user/controller/UserController.java
+++ src/main/java/com/mooddy/backend/feature/user/controller/UserController.java
@@ -1,18 +1,14 @@
 package com.mooddy.backend.feature.user.controller;
 
 import com.mooddy.backend.feature.user.domain.User;
+import com.mooddy.backend.feature.user.dto.OnboardingRequest;
 import com.mooddy.backend.feature.user.dto.UserSearchResponseDto;
-import com.mooddy.backend.feature.user.repository.UserRepository;
+import com.mooddy.backend.feature.user.service.UserOnboardingService;
 import com.mooddy.backend.feature.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,17 +17,15 @@ import java.util.List;
 @RequestMapping("/api/users")
 public class UserController {
 
-    private final UserRepository userRepository;
     private final UserService userService;
+    private final UserOnboardingService userOnboardingService;
 
-    @PatchMapping("/{id}/onboarding") // 상태만 전달
-    public ResponseEntity<Void> completedOnboarding(@PathVariable Long id) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
-        user.setOnboardingCompleted(true);
-
-        userRepository.save(user);
+    @PatchMapping("/onboarding")
+    public ResponseEntity<Void> completedOnboarding(
+            @AuthenticationPrincipal User user,
+            @RequestBody OnboardingRequest request
+    ) {
+        userOnboardingService.completedOnboarding(user.getId(), request);
 
         return ResponseEntity.ok().build();
     }
