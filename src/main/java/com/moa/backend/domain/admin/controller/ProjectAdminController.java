e7936bf969000cf6f9e8d4be156e71bf687de641
sdw98 <sdw9851@gmail.com>
2025-11-12 17:23:48 +0900

    refacotr(project): api 변경

--- src/main/java/com/moa/backend/domain/admin/controller/ProjectAdminController.java
+++ src/main/java/com/moa/backend/domain/admin/controller/ProjectAdminController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/admin/project")
+@RequestMapping("/admin/project")
 @RequiredArgsConstructor
 @PreAuthorize("hasRole('ADMIN')")
 public class ProjectAdminController {
