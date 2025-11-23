23eec7e686ef6f62edce6175c164caa3003d65e6
skymoon7337 <skymoon7337@gmail.com>
2025-11-24 03:34:28 +0900

    fix: revenue 수익표시 세분화

--- src/main/java/com/moa/backend/domain/admin/dto/statistics/revenue/PlatformRevenueDto.java
+++ src/main/java/com/moa/backend/domain/admin/dto/statistics/revenue/PlatformRevenueDto.java
@@ -18,5 +18,8 @@ public class PlatformRevenueDto {
     private Long platformFeeAmount;
     private Double platformFeeRate;
     private Long otherCosts;
-    private Long netProfit;
+    // 메이커 지급액(총 결제액 - PG 수수료 - 플랫폼 수수료)
+    private Long netPayoutToMaker;
+    // 플랫폼 순이익(플랫폼 수수료 - PG 수수료 - 기타 비용)
+    private Long netPlatformProfit;
 }
